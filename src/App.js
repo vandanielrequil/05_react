50ffa50c4d8ff93928f956be308f2ccd7f61eefc
Сухомлинов Данил Игоревич <dsuhomlinov@bostonsd.ru>
2021-08-15 16:20:48 +0300

    workin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles.css';
+
 
 function App() {
+
+  const [message, writeMsg] = useState('');
+  const [msgArr, msgArrF] = useState([]);
+
+
+  function sendMsg(e) {
+    if (message) { msgArrF(a => [...a, message]); console.log('Here1') }
+    else { console.log('Here2'); return };
+    console.log(msgArr);
+    msgArr.forEach(a => console.log(a));
+    writeMsg('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="messenger">
+
+        <div className="bg"></div>
+        <div className="chat"></div>
+
+        <form className="sendform" action="">
+          <textarea className="sendtext" id="txta1" cols="30" rows="5" value={message} onChange={e => writeMsg(e.target.value)}></textarea>
+          <button className="sendbut" type='button' onClick={e => sendMsg(e)} >Отправить</button>
+        </form>
+
+      </div>
+    </main>
   );
 }
 
