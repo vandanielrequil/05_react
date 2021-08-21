f000235bf45b1925aba4d6f8087e3dce0c29d3bd
Сухомлинов Данил Игоревич <dsuhomlinov@bostonsd.ru>
2021-08-21 18:11:51 +0300

    lesson_02

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, { useState } from 'react';
-import './styles.css';
-
+import React, { useState } from "react";
+import "./styles.css";
+import CompSendMsg from "./CompSendMsg";
 
 function App() {
 
-  const [message, writeMsg] = useState('');
-  const [msgArr, msgArrF] = useState([]);
-
-
-  function sendMsg(e) {
-    if (message) { msgArrF(a => [...a, message]); console.log('Here1') }
-    else { console.log('Here2'); return };
-    console.log(msgArr);
-    msgArr.forEach(a => console.log(a));
-    writeMsg('');
-  }
+  const [message, writeMsg] = useState("");
+  let [msgArr, msgArrF] = useState([]);
 
   return (
     <main>
       <div className="messenger">
-
         <div className="bg"></div>
-        <div className="chat"></div>
-
+        <div className="chat">{msgArr}</div>
         <form className="sendform" action="">
-          <textarea className="sendtext" id="txta1" cols="30" rows="5" value={message} onChange={e => writeMsg(e.target.value)}></textarea>
-          <button className="sendbut" type='button' onClick={e => sendMsg(e)} >Отправить</button>
+          <textarea
+            className="sendtext"
+            id="txta1"
+            cols="30"
+            rows="5"
+            value={message}
+            onChange={(e) => writeMsg(e.target.value)}
+          ></textarea>
+          <button className="sendbut" type="button" onClick={(e) => msgArrF = CompSendMsg(message,msgArrF)}>
+            Отправить
+          </button>
         </form>
-
       </div>
     </main>
   );
---
+++ src/CompSendMsg.js
@@ -0,0 +1,31 @@
+const CompSendMsg = function CompSendMsg(message,msgArrF) {
+
+    function sendAns() {
+    const ansArr = [
+      "Я выпотрошу тебя и всю твою семью",
+      "Твоя душа принадлежит мне",
+      "И не надейся на быструю смерть",
+      "Да начнётся истребление",
+    ];
+    let timer;
+    function answerMsg() {
+      let ansNum = parseInt(Math.random() * ansArr.length);
+      let ans = <div className="message message__answer">{ansArr[ansNum]}</div>;
+      msgArrF((a) => [...a, ans]);
+      return clearInterval(timer);
+    }
+    timer = setInterval(() => answerMsg(), 1000);
+  }
+
+  if (message) {
+    let mess = <div className="message message__send">{message}</div>;
+    msgArrF((a) => [...a, mess]);
+  } else {
+    return 0;
+  }
+  //writeMsg("");
+  sendAns();
+  return msgArrF;
+};
+
+export default CompSendMsg;
